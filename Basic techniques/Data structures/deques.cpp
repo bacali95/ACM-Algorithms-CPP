12a4947cf5f3b90570110955b17e4cad76307d78
bacali95 <nasreddine.bacali95@gmail.com>
2018-11-17 22:52:32 +0100

    add test deque

--- Basic techniques/Data structures/deques.cpp
+++ Basic techniques/Data structures/deques.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main(int argc, char **argv) {
     deque<int> d;
-    d.push_back(5);
-    d.push_front(6);
-    d.pop_back();
-    d.pop_front();
+    d.push_back(5); // [5]
+    d.push_front(6); // [6,5]
+    d.pop_back(); // [6]
+    d.pop_front(); // []
     return 0;
 }
