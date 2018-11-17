65416e8ad725b36caa662fd4f65378bf9cc11a5d
bacali95 <nasreddine.bacali95@gmail.com>
2018-11-17 13:59:00 +0100

    Initial commit

---
+++ template.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+/*
+ * This is the main template for all Contests Submissions
+ */
+
+typedef long long ll;
+
+#define pb push_back
+#define mp make_pair
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+#ifndef ONLINE_JUDGE
+    freopen("input.in", "r", stdin);
+#else
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#endif
+    //Code here...
+    return 0;
+}
+
